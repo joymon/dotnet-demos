63b7dc17852e76506a563fd6369c3794de045371
joy.g <joy.g@JOYG9470M>
2017-08-10 20:42:03 -0400

    Initial checkin

---
+++ data/file-formats/parquet/ParquetFileManipulation/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ParquetFileManipulation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ParquetDataGrid = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.FileNametextBox = new System.Windows.Forms.TextBox();
+            this.LoadRefDataButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ParquetDataGrid)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ParquetDataGrid
+            // 
+            this.ParquetDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ParquetDataGrid.Location = new System.Drawing.Point(12, 53);
+            this.ParquetDataGrid.Name = "ParquetDataGrid";
+            this.ParquetDataGrid.Size = new System.Drawing.Size(463, 196);
+            this.ParquetDataGrid.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.LoadRefDataButton);
+            this.groupBox1.Controls.Add(this.FileNametextBox);
+            this.groupBox1.Controls.Add(this.LoadButton);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(462, 43);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(251, 14);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(75, 23);
+            this.LoadButton.TabIndex = 0;
+            this.LoadButton.Text = "Load";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // FileNametextBox
+            // 
+            this.FileNametextBox.Location = new System.Drawing.Point(6, 16);
+            this.FileNametextBox.Name = "FileNametextBox";
+            this.FileNametextBox.Size = new System.Drawing.Size(195, 20);
+            this.FileNametextBox.TabIndex = 1;
+            this.FileNametextBox.Text = "C:\\Temp\\Data\\customer.impala.parquet";
+            // 
+            // LoadRefDataButton
+            // 
+            this.LoadRefDataButton.Location = new System.Drawing.Point(332, 13);
+            this.LoadRefDataButton.Name = "LoadRefDataButton";
+            this.LoadRefDataButton.Size = new System.Drawing.Size(100, 23);
+            this.LoadRefDataButton.TabIndex = 2;
+            this.LoadRefDataButton.Text = "Load Ref Data";
+            this.LoadRefDataButton.UseVisualStyleBackColor = true;
+            this.LoadRefDataButton.Click += new System.EventHandler(this.LoadRefDataButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 293);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.ParquetDataGrid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ParquetDataGrid)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView ParquetDataGrid;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.TextBox FileNametextBox;
+        private System.Windows.Forms.Button LoadRefDataButton;
+    }
+}
+
---
+++ data/file-formats/parquet/ParquetFileManipulation/Form1.cs
@@ -0,0 +1,62 @@
+﻿using Parquet;
+using Parquet.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParquetFileManipulation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+
+            using (Stream fs = File.OpenRead(FileNametextBox.Text))
+            {
+                using (var reader = new ParquetReader(fs))
+                {
+                    var ds = reader.Read();
+                    ParquetDataGrid.DataSource = GetDataSetFromParquet( ds);
+                }
+            }
+        }
+        private DataSet GetRefDataSet()
+        {
+            var ds = new DataSet(
+    new SchemaElement<int>("id"),
+    new SchemaElement<string>("city")
+);
+            ds.Add(1, "London");
+            ds.Add(2, "Derby");
+            return ds;
+        }
+
+        private void LoadRefDataButton_Click(object sender, EventArgs e)
+        {
+            ParquetDataGrid.AutoGenerateColumns = true;
+            //RawValues is an array. It will have 2 elements.
+            //TODO convert the array elements as object
+            ParquetDataGrid.DataSource = GetRefDataSet().Select(s =>  new { id=s.RawValues[0],city=s.RawValues[1] }).ToList();
+            ParquetDataGrid.Refresh();
+            foreach (var i in GetRefDataSet())
+            {
+
+            }
+        }
+        System.Data.DataSet GetDataSetFromParquet(DataSet pDataSet)
+        {
+            return null;
+        }
+    }
+}
