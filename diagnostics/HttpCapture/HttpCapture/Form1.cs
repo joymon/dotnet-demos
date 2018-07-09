fc40aacbfe1088716a40ab8adb3df637587d54d1
joymon <joymon@gmail.com>
2018-07-09 18:02:32 -0400

    HttpCapture using DiagnosticListener

---
+++ diagnostics/HttpCapture/HttpCapture/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+
+namespace HttpCapture
+{
+    public partial class Form1 : Form
+    {
+        private TextBoxTraceListener _textBoxListener;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void MakeHttpCallbutton_Click(object sender, EventArgs e)
+        {
+            WebClient wc = new WebClient();
+            string s = await wc.DownloadStringTaskAsync(@"https://api.pi.delivery/v1/pi?start=0&numberOfDigits=5");
+            MessageBox.Show($"Downloaded using WebClient - {s}");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            HttpListener list = new HttpListener();
+            DiagnosticListener.AllListeners.Subscribe(list);
+            _textBoxListener = new TextBoxTraceListener(LogstextBox);
+            Trace.Listeners.Add(_textBoxListener);
+        }
+
+        private async void MakeHttpCallUsingHttpClientbutton_Click(object sender, EventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            string s = await client.GetStringAsync(@"https://api.pi.delivery/v1/pi?start=0&numberOfDigits=6");
+            MessageBox.Show($"Downloaded using HttpClient - {s}");
+        }
+    }
+}
---
+++ diagnostics/HttpCapture/HttpCapture/HttpObserver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+
+namespace HttpCapture
+{
+    class HttpObserver : IObserver<KeyValuePair<string, object>>
+    {
+        void IObserver<KeyValuePair<string, object>>.OnCompleted()
+        {
+            //throw new NotImplementedException();
+        }
+
+        void IObserver<KeyValuePair<string, object>>.OnError(Exception error)
+        {
+            //throw new NotImplementedException();
+        }
+
+        void IObserver<KeyValuePair<string, object>>.OnNext(KeyValuePair<string, object> value)
+        {
+            if (value.Key.Equals("System.Net.Http.Desktop.HttpRequestOut.Start"))
+            {
+                HttpWebRequest req = value.Value.GetType().GetProperty("Request").GetValue(value.Value) as HttpWebRequest;
+                if (req != null)
+                {
+                    Trace.WriteLine($"Http outgoing intercepted - Destination {req.Address}");
+                }
+            }
+            //throw new NotImplementedException();
+        }
+    }
+}
