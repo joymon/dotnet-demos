0e0b3efb12b6d9a4faed51ce42f4962200c50090
joymon <joymon@gmail.com>
2018-07-10 00:18:11 -0400

    Refactoring

--- diagnostics/HttpCapture/HttpCapture/HttpObserver.cs
+++ diagnostics/HttpCapture/HttpCapture/HttpObserver.cs
@@ -9,16 +9,17 @@ namespace HttpCapture
     {
         void IObserver<KeyValuePair<string, object>>.OnCompleted()
         {
-            //throw new NotImplementedException();
+            //Do nothing
         }
 
         void IObserver<KeyValuePair<string, object>>.OnError(Exception error)
         {
-            //throw new NotImplementedException();
+            //Do nothing
         }
 
         void IObserver<KeyValuePair<string, object>>.OnNext(KeyValuePair<string, object> value)
         {
+            // This function triggered for all events in the "System.Net.Http" area. Filter based on use case.
             if (value.Key.Equals("System.Net.Http.Desktop.HttpRequestOut.Start"))
             {
                 HttpWebRequest req = value.Value.GetType().GetProperty("Request").GetValue(value.Value) as HttpWebRequest;
