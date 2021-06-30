012f51e39a8f2ef1bde6f6f00cf7d99b64687eaf
Joy George Kunjikkuru <joymon@gmail.com>
2021-06-29 20:41:10 -0400

    Initial check in of Mutiple WebAPI controller assemblies demo

---
+++ web/webapi-multiple-controller-assemblies/AnotherControllersAssembly/AnotherAPIController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace AnotherControllersAssembly
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnotherAPIController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "AnotherAPI - value1", "AnotherAPI - value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+    }
+}
---
+++ web/webapi-multiple-controller-assemblies/HostingAssembly/Startup.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webapi_multiple_controller_assemblies
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSwaggerGen(genOptions => {
+                genOptions.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { 
+                Version = "v1",
+                Title = "Multiple Controllers Assemblies",
+                Description = "Demo to show how multiple controller assemblies can be loaded into same WebAPI application",
+             
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(options => {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Mutiple Controller Assemblies");
+            });
+        }
+    }
+}
