42e8e2f6268f230ff2aa6fef27c4083713827804
Joy George Kunjikkuru <joymon@gmail.com>
2021-08-07 15:30:39 -0400

    Added echo controller

--- web/webapi/simple-k8s-hosting/SimpleK8sHosting/Controllers/WeatherForecastController.cs
+++ web/webapi/simple-k8s-hosting/SimpleK8sHosting/Controllers/WeatherForecastController.cs
@@ -1,10 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
 namespace SimpleK8sHosting.Controllers
 {
     [ApiController]
